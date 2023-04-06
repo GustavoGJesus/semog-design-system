6c03ea38a52a25f56f0a976bcadff9f95b52635a
GustavoGJesus <ggomesdejesus01@gmail.com>
2023-04-06 16:05:34 -0300

    feat: configure stitches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,6 +1,13 @@
-import { colors } from '@semog-ui/tokens'
+import { styled } from './styles'
 import React from 'react'
 
+const Button = styled('button', {
+  fontFamily: '$default',
+  background: '$ignite500',
+  borderRadius: '$md',
+  padding: '$4',
+})
+
 export function App() {
-  return <h1 style={{ color: colors.blue }}>Hello world!</h1>
+  return <Button>Hello world!</Button>
 }
